1acf3f04642daad3d12640565180828db28d0c3c
Lumiat <grace20177102@icloud,com>
2026-04-23 12:39:21 +0800

    add arg for crossover+mutation configuration

--- assignment1/main.m
+++ assignment1/main.m
@@ -1,9 +1,22 @@
-function main(funcID)
+function main(funcID, algoType)
+    %% hyper params
+    numRuns = 30;
+    maxGen = 100;
+    popSize = 50;
+    pc = 0.8;
+    pm = 0.1;
+    eliteRate = 0.1;
+    eta_c = 10;
+    eta_m = 10;
+
     % funcID: if 1 use dejong1, if 2 use dejong2
     
     if nargin < 1
         funcID = 1;
     end
+    if nargin < 2
+        algoType = 2; 
+    end 
 
     %% configure according to funcID
     if funcID == 1
@@ -20,21 +33,28 @@ function main(funcID)
 
     fprintf('Current object function: %s\n', funcName);
 
-    %% hyper params
-    numRuns = 30;
-    maxGen = 100;
-    popSize = 50;
-    pc = 0.8;
-    pm = 0.1;
-    eliteRate = 0.1;
-    
+    if algoType == 1
+        % use arithmetic crossover + gaussian mutation
+        crossOp = @arithmetic_crossover;
+        mutOp = @gaussian_mutation;
+        algoName = 'Basic (Arithmetic + Gaussian)';
+    else
+        % use sbx+polinomial mutation
+        crossOp = @(p1, p2, pc, lb, ub) sbx_crossover(p1, p2, pc, eta_c, lb, ub);
+        mutOp = @(x, pm, lb, ub) polynomial_mutation(x, pm, eta_m, lb, ub);
+        algoName = 'Advanced (SBX + Polynomial)';
+    end
+
+    fprintf('Current object function: %s\n', funcName);
+    fprintf('Current Algorithm: %s\n', algoName);
+
     %% 30 runs
     allHistories = zeros(maxGen, numRuns);
     finalResults = zeros(numRuns, 1);
     
     fprintf("Running Algorithm...\n");
     for r = 1:numRuns
-        [history, bestVal] = genetic_algorithm(targetFunc, bounds, popSize, maxGen, pc, pm, eliteRate);
+        [history, bestVal] = genetic_algorithm(targetFunc, bounds, popSize, maxGen, pc, pm, eliteRate, crossOp, mutOp);
         allHistories(:, r) = history;
         finalResults(r) = bestVal;
     end
@@ -50,7 +70,11 @@ function main(funcID)
     plot(1:maxGen, allHistories(:, 5), 'LineWidth',1.5); % show the fifth run
     xlabel('Generation');
     ylabel('Fitness');
-    title('Single Run Evolution Process (De Jong 2, 5th run)');
+    if funcID == 1
+        title('Single Run Evolution Process (De Jong 1, 5th run)');
+    else
+        title(['Single Run Evolution Process (De Jong 2, 5th run)']);
+    end
     grid on;
     
     %% draw boxplot
